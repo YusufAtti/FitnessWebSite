9be19e032c584b07395f5309f2066e338f56b27b
YusufAtti <158186024+YusufAtti@users.noreply.github.com>
2024-02-11 14:24:26 +0300

    Update mailHelper.js

--- A/helper/mailHelper.js
+++ A/helper/mailHelper.js
@@ -6,13 +6,13 @@ const sendEmail = async (toEmail, subject, message) => {
     const transporter = nodemailer.createTransport({
       service: 'Gmail', // E-posta sağlayıcınıza göre değiştirin
       auth: {
-        user: 'omer.aydn.222@gmail.com', // E-posta adresiniz
-        pass: 'sesq ucnp czfa appl' // E-posta şifreniz
+        user: 'tempmail@gmail.com', // E-posta adresiniz
+        pass: 'temppassword' // E-posta şifreniz
       }
     });
 
     const mailOptions = {
-      from: 'omer.aydn.222@gmail.com', // Gönderici e-posta adresi
+      from: 'tempmail@gmail.com', // Gönderici e-posta adresi
       to: toEmail, // Alıcı e-posta adresi
       subject: subject, // E-posta konusu
       html: message // E-posta içeriği
